d7145d132fc9e1ceb3646b45241c26c60145c047
Stephen Call <stephen@calls4.com>
2016-09-11 09:47:26 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TopFootballer::Application.config.secret_token = '527be2bdb7311254ff902c158bda61abc6d9f5ce3d5509ce1ac547d40d30bb7f33e53c0537175edce15c2cf3f4465518cc816ebfe0a3ea1d66a4998c4d51e369'
